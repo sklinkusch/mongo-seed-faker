3de6a5c623cffef6fdd1841744da0c64cbc5d96f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-05 15:22:48 +0200

    node script working

---
+++ index.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/faker', { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  age: Number,
+  username: String,
+  shortBio: String,
+  streetAddress: String,
+  zip: String,
+  city: String,
+  country: String,
+  phone: String
+})
+
+const User = mongoose.model('User', userSchema);
+
+const id = process.argv[2];
+User.findById(id, (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+  }
+})
