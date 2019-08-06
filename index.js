93b15e5d8c2608a24721c6e45faf6236babb7e51
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-06 13:30:31 +0200

    modify file structure

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const userIDRoute = require('./router');
-const { generateDB } = require('./faker');
-const { allUsers } = require('./mongoose');
+const userIDRoute = require('./model/router');
+const { generateDB } = require('./model/faker');
+const { allUsers } = require('./model/mongoose');
 
 generateDB();
 
