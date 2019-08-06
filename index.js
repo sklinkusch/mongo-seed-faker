73b15aeb7d6b85f9b12c8f11864117237384e974
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-06 08:58:02 +0200

    finissh api

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const userIDRoute = require('./router');
+const { generateDB } = require('./faker');
+
+generateDB();
 
 app.use('/user', (req, res) => userIDRoute(req, res));
 
