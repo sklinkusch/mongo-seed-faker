d8a5e603ca80109bd087d97d5d757d42b5dfe2c9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-06 10:48:42 +0200

    addd timestamps

--- schema.js
+++ schema.js
@@ -16,7 +16,10 @@ const userSchema = new mongoose.Schema({
     country: String
   },
   phone: String
-});
+}, {
+    strict: "throw",
+    timestamps: true
+  });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
